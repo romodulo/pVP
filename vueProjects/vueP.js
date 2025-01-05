59b1c182c6ef3fe8d8b8b1bad8d9592fc4ee44b4
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-01-05 22:06:24 +1000

    stable <22:06>

--- vueProjects/vueP.js
+++ vueProjects/vueP.js
@@ -5,8 +5,8 @@ const {createApp, ref} = Vue
 createApp( {
     setup() {
         const msg = ref(0)
-        const increment = () => {
-            msg.value += 1
+        const increment = function superIncrement () {
+            return msg.value += 1
         }
         return {
             msg, increment
